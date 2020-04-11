329b90999101b7337dd56d20d7255491020e1bd6
Nabil Hayet <nabil.bracu@gmail.com>
2020-04-11 01:45:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,28 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false 
   end
+
 
   def question?
-
+    self.end_with?("?") ? true : false 
   end
+
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
+
 
   def count_sentences
-
+    arr=[]
+  arr=self.split(/\.|\?|\!/)
+  if arr.length ==0 
+    return 0 
+  else 
+    arr.delete_if{|str| str==""}
+    arr.length
   end
 end
+end 
